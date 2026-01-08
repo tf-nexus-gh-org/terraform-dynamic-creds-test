85664087a0024e02da33bda76f8a96c61f514480
Chris Griggs <cgriggs@hashicorp.com>
2026-01-08 10:46:05 -0800

    Modify AWS provider version and add IAM policy
    Updated AWS provider version and added IAM policy resource.

--- main.tf
+++ main.tf
@@ -2,17 +2,40 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.0"
+      version = "~> 5.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
     }
   }
 }
 
 provider "aws" {
-  # No credentials configured for OIDC test 
-  # relies on dynamic credentials from HCP Terraform
+  # No credentials configured - relies on dynamic credentials from HCP Terraform
   region = "us-east-1"
 }
 
 data "aws_caller_identity" "current" {}
 
 data "aws_region" "current" {}
+
+resource "random_id" "policy_suffix" {
+  byte_length = 4
+}
+
+resource "aws_iam_policy" "test_write_permission" {
+  name        = "terraform-dynamic-creds-test-policy-${random_id.policy_suffix.hex}"
+  description = "Test policy to verify write permissions - created by terraform-dynamic-creds-test"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Deny"
+        Action   = "*"
+        Resource = "*"
+      }
+    ]
+  })
+}
